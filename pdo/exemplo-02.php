61d39e0385572e1fd2e5604c67d6632fd2608cc7
Thomaz <thomazcl1@hotmail.com>
2018-03-09 18:16:18 -0300

    Acrescimo de pdo

---
+++ pdo/exemplo-02.php
@@ -0,0 +1,28 @@
+<?php
+
+//Conexão SQL Server
+//$conn = new PDO("sqlsrv:database=dbphp7;server=localhost;ConnectionPooling=0", "login", "senha");
+//com ODBC
+$conn = new PDO("odbc:sqlPHP","login","senha");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row) {
+	foreach ($row as $key => $value) {
+		echo "<strong>".$key."</strong> = ".$value." | ";
+	}
+	echo "<br/>";
+}
+
+
+//var_dump($results);
+
+
+
+
+
+?>
